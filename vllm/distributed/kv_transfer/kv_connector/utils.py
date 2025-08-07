c22790917c239f3e0a3a9ac2bdac4cc44937b0ce
Schwinn Saereesitthipitak <schwinns@nvidia.com>
2025-08-07 08:08:10 +0000

    Patch kv output vllm bug

--- vllm/distributed/kv_transfer/kv_connector/utils.py
+++ vllm/distributed/kv_transfer/kv_connector/utils.py
@@ -143,10 +143,13 @@ class KVOutputAggregator:
         finished_recving = set[str]()
         for output in outputs:
             output = output.kv_connector_output
-            update_finished_set(output.finished_sending,
-                                self._send_remaining_count, finished_sending)
-            update_finished_set(output.finished_recving,
-                                self._recv_remaining_count, finished_recving)
+            # Handle case where kv_connector_output is None
+            # This can happen when KV transfer group is not properly initialized
+            if output is not None:
+                update_finished_set(output.finished_sending,
+                                    self._send_remaining_count, finished_sending)
+                update_finished_set(output.finished_recving,
+                                    self._recv_remaining_count, finished_recving)
 
         # select output of the worker specified by output_rank
         output = outputs[output_rank]
